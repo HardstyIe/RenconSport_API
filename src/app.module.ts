47f5e74aa964d8b8968530698ff979181ba62e4e
HardstyIe <Hardstyle.dd@gmail.com>
2023-09-04 15:11:10 +0200

    docs(readme):add of first version of readme

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'renconSportAPI',
+      password: 'f6KA@2,9Em[vbDF;~dP/VjrqFaUD',
+      database: 'renconsport',
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
