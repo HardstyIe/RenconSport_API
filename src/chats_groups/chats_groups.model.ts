e92c97f119b44cd8bd54285c3d655809b6c51aaa
HardstyIe <Hardstyle.dd@gmail.com>
2023-09-08 20:26:51 +0200

    feat(api): add some routes for api

--- src/chats_groups/chats_groups.model.ts
+++ src/chats_groups/chats_groups.model.ts
@@ -1,6 +1,9 @@
 import { Prisma } from '@prisma/client';
 
-export interface ChatGroup implements Prisma.ChatGroupCreateInput {
+export class ChatGroup implements Prisma.ChatGroupCreateInput {
+  training: Prisma.TrainingCreateNestedOneWithoutChatGroupsInput;
+  UserChatGroups?: Prisma.UserChatGroupCreateNestedManyWithoutChat_groupInput;
+  ChatMessages?: Prisma.ChatMessageCreateNestedManyWithoutChat_groupInput;
   id: number;
   name: string;
   training_id?: number;
